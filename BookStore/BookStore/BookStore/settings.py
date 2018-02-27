7a576fec856c92e2451708d024f1d3c72fe97b64
Hala Elhamahmy <hala.elhamahmy@gmail.com>
2018-02-28 01:41:22 +0200

    Update settings.py

--- BookStore/BookStore/BookStore/settings.py
+++ BookStore/BookStore/BookStore/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c)mi$^#l)r-atfok2@$edmk2ra4mpkb^(@a85n)i+(ngxer(1z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
